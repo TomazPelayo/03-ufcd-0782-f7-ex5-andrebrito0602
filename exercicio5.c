90e66810c32868d730c14622c3709c189a54ae9a
andrebrito0602 <122288809+andrebrito0602@users.noreply.github.com>
2023-02-27 14:43:46 +0000

    .

--- exercicio5.c
+++ exercicio5.c
@@ -1,2 +1,14 @@
 // Escreva o seu código a seguir
+#include<stdio.h>
 
+int main()  {
+    float n1,n2,res;
+    printf("Qual � o teu peso?\n");
+    scanf("%f", &n1);
+    printf("Qual � a tua altura?\n");
+    scanf("%f", &n2);
+    
+    res=n1/(n2*n2);
+    printf("O teu �ndice de massa corporal �  %.3f",res);
+   
+}
